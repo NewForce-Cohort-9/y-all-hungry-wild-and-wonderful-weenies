e9ff3fb381a1046b64abc1dce9f03f204fbf5d0f
Michael Adkins <michaelpauladkins@gmail.com>
2024-05-15 11:28:27 -0400

    saved updates and committing before checking a pull request

--- scripts/desserts.js
+++ scripts/desserts.js
@@ -2,7 +2,7 @@ export const DessertSelector = async () => {
     const response = await fetch("http://localhost:8088/desserts")
     const desserts = await response.json()
     
-    let dessertHTML = `<select id="dessert"><option value="0">Select Dessert</option><option value="none">None</option>`
+    let dessertHTML = `<select id="dessert"><option value="0">Select Dessert</option>`
     const divStringArray = desserts.map(
         (dessert) => {
             return `<option value="${dessert.id}">${dessert.name}</option>`
