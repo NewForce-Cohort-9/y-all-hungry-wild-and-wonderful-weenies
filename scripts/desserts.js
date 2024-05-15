3bd1c70eea98ea69fb881a100d34ef072587a595
Michael Adkins <michaelpauladkins@gmail.com>
2024-05-15 13:39:33 -0400

    I revised the defective mess I was creating and prepared for my next pull request.

--- scripts/desserts.js
+++ scripts/desserts.js
@@ -1,16 +1,28 @@
-export const DessertSelector = async () => {
+import { setDessert } from "./transientState.js"
+
+const handleDessertChoice = (changeEvent) => {
+    if (changeEvent.target.id === "desserts") {
+        setFood(parseInt(changeEvent.target.value))
+    }
+ }
+
+export const DessertDropdown = async () => {
     const response = await fetch("http://localhost:8088/desserts")
     const desserts = await response.json()
-    
-    let dessertHTML = `<select id="dessert"><option value="0">Select Dessert</option>`
-    const divStringArray = desserts.map(
-        (dessert) => {
-            return `<option value="${dessert.id}">${dessert.name}</option>`
-        },
-    )
-    
-    dessertHTML += divStringArray.join("")
-    dessertHTML += `</select>`
 
-    return dessertHTML   
+    document.addEventListener("change", handleDessertChoice)
+
+    let dessertOptionsHTML = `<select class="form-select form-select-md mb-3" id='desserts'>
+    <option value='0'>Select Your Dessert...</option>`
+
+    const divStringArray = await desserts.map(
+        (dessert) => {
+          return `<option value='${dessert.id}'>${dessert.name}</option>`
+        }
+    )
+
+    dessertOptionsHTML += divStringArray.join("")
+    dessertOptionsHTML += `</select>`
+
+    return dessertOptionsHTML
 }
